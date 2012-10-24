183bfaec8cca91d0efcb4be10e1487ea8427cc54
Andrew Campey <andrew.campey@gmail.com>
2012-10-24 18:11:47 +1100

    Intial commit

---
+++ basicCRM/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicCRM::Application.config.secret_token = '60d1b505822a452e51d3f0965f0f3ee3bb8bad5d3ac6603ec979063291281bf1b8aa90ee73b1eb3427bcaaab0b2799d930f88a2d3cdbdce8192a28d94e872db1'
