73b442d14d921e154de994fa536215c46599311f
Andrew Campey <andrew.campey@gmail.com>
2012-10-28 21:07:45 +1100

    Initial commit

---
+++ pb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pb::Application.config.secret_token = '141c1bb21f0629678af18be6bcbd8ba75dabfd2904568014628b8c1172fd242d47b8f5a5d52900a21b3865b9b0d1f1968240f6f692ffee525da35f1a73f54dda'
